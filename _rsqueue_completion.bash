254e5c28246c1874b5c4f2173bd8d29acafc55f0
nanoseb <sl2v17@soton.ac.uk>
2019-09-06 11:57:51 +0200

    add qcd in autocompletion script

--- _rsqueue_completion.bash
+++ _rsqueue_completion.bash
@@ -5,13 +5,13 @@
 
 _rsqueue() 
 {
-    local cur prev opts base
+    local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     #  Basic arguments to complete
-    opts="-i --user --top --part --count -a --all --storage --run --cost --memory --maxit --st --pn --node --tree --residuals --progress-bar -d --stop --follow --notif --export-conf --version --help"
+    opts="--info --user --top --part --count -a --all --storage --run --cost --memory --maxit --st --pn --node --tree --residuals --progress-bar -d --stop --follow --notif --export-conf --version --help"
 
 
     #  Dynamic completion of arguments
@@ -31,7 +31,7 @@ _rsqueue()
         -f|--follow)
             local timelist="5 15 30 120"
 
-            COMPREPLY=( $(compgen -W "${timelist}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${timelist} ${opts}" -- ${cur}) )
             return 0
             ;;
         *)
@@ -43,3 +43,24 @@ _rsqueue()
 }
 
 complete -F _rsqueue rsqueue
+
+
+# Function to move to a computation directory via its jobID
+qcd () {
+    echo "Moving to $1"
+    cd "$(rsqueue -d $1)"
+}
+
+
+_qcd()
+{
+    local cur jobid
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    jobid=$(squeue -u $USER -o "%i" -h)
+
+    COMPREPLY=( $(compgen -W "${jobid}" -- ${cur}) )
+    return 0
+}
+
+complete -F _qcd qcd 
