2b55b805e31574cb9735e0272bbdb4c866fd75ce
nanoseb <sl2v17@soton.ac.uk>
2019-09-16 16:50:51 +0200

    improve qcd autocompletion

--- _rsqueue_completion.bash
+++ _rsqueue_completion.bash
@@ -3,10 +3,8 @@
 # by Sebastien Lemaire <sebastien.lemaire@soton.ac.uk>
 # license MIT
 
-_rsqueue() 
-{
+_rsqueue() {
     local cur prev opts
-    COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
@@ -14,32 +12,28 @@ _rsqueue()
     opts="--info -i --user -u --top -t --part -p --count -c --all -a --storage -s --run -r --cost --memory --maxit --st --pn \
           --node --tree --residuals --progress-bar -dir -d --stop --follow -f --notif --export-conf --version -v --help -h"
 
-
     #  Dynamic completion of arguments
     case "${prev}" in
         -d|--dir|--stop)
             local jobid=$(squeue -u $USER -o "%i" -h)
 
             COMPREPLY=( $(compgen -W "${jobid}" -- ${cur}) )
-            return 0
             ;;
         -u|--user)
             local userlist=$(squeue -o "%u" -h | sort | uniq)
 
             COMPREPLY=( $(compgen -W "${userlist}" -- ${cur}) )
-            return 0
             ;;
         -f|--follow)
             local timelist="5 15 30 120"
 
             COMPREPLY=( $(compgen -W "${timelist} ${opts}" -- ${cur}) )
-            return 0
             ;;
         *)
+            COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
             ;;
     esac
 
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
     return 0
 }
 
@@ -48,7 +42,7 @@ complete -F _rsqueue rsqueue
 
 # Function to move to a computation directory via its jobID
 qcd () {
-    local job_working_directory=$(rsqueue -d $1)
+    local job_working_directory=$(rsqueue -d ${1%%-*})
     if [ -d "$job_working_directory" ]
     then
         echo "Moving to $job_working_directory"
@@ -57,12 +51,10 @@ qcd () {
 }
 
 
-_qcd()
-{
+_qcd() {
     local cur jobid
-    COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    jobid=$(squeue -u $USER -o "%i" -h)
+    jobid=$(squeue -u $USER -o "%i-%Z" -h)
 
     COMPREPLY=( $(compgen -W "${jobid}" -- ${cur}) )
     return 0
