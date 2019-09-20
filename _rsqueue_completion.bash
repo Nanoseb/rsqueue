08cf6128f0491fd5431c9d1858bd9e48dfcf0298
nanoseb <sl2v17@soton.ac.uk>
2019-09-20 11:08:30 +0200

    add jcd and qcd, and unload function

--- _rsqueue_completion.bash
+++ _rsqueue_completion.bash
@@ -10,7 +10,7 @@ _rsqueue() {
 
     #  Basic arguments to complete
     opts="--info -i --user -u --top -t --part -p --count -c --all -a --storage -s --run -r --cost --memory --maxit --st --pn \
-          --node --tree --residuals --progress-bar -dir -d --stop --follow -f --notif --export-conf --version -v --help -h"
+          --node --tree --residuals --progress-bar --dir -d --stop --follow -f --notif --export-conf --version -v --help -h"
 
     #  Dynamic completion of arguments
     case "${prev}" in
@@ -30,7 +30,7 @@ _rsqueue() {
             COMPREPLY=( $(compgen -W "${timelist} ${opts}" -- ${cur}) )
             ;;
         *)
-            COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )  
             ;;
     esac
 
@@ -40,8 +40,9 @@ _rsqueue() {
 complete -F _rsqueue rsqueue
 
 
-# Function to move to a computation directory via its jobID
-qcd () {
+# jcd:
+# Move to a job working directory from its jobID
+jcd () {
     local job_working_directory=$(rsqueue -d ${1%%-*})
     if [ -d "$job_working_directory" ]
     then
@@ -50,14 +51,66 @@ qcd () {
     fi
 }
 
-
-_qcd() {
+_jcd() {
     local cur jobid
     cur="${COMP_WORDS[COMP_CWORD]}"
-    jobid=$(squeue -u $USER -o "%i-%Z" -h)
+    jobid=$(squeue -u $USER -o "%i" -h)
 
     COMPREPLY=( $(compgen -W "${jobid}" -- ${cur}) )
     return 0
 }
 
-complete -F _qcd qcd 
+complete -F _jcd jcd 
+
+
+# qcd:
+# Wrapper of cd to more to a job working directory, autocompletion of only job directories
+qcd () {
+    if [ -d "$1" ]
+    then
+        cd "$1"
+    fi
+}
+
+_qcd() {
+    local cur paths
+    local IFS=$'\n'
+
+    # Output one suggestion per line
+    local width=$(bind -v | sed -n 's/^set completion-display-width //p')
+
+    if [[ $width -ne 0 ]]; then
+        # change the readline variable
+        bind "set completion-display-width 0"
+
+        # set up PROMPT_COMMAND to reset itself to its current value
+        PROMPT_COMMAND="PROMPT_COMMAND=$(printf %q "$PROMPT_COMMAND")"
+
+        # set up PROMPT_COMMAND to reset the readline variable
+        PROMPT_COMMAND+="; bind 'set completion-display-width $width'"
+    fi
+
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    paths=$(squeue -u $USER -o "%Z" -h)
+
+    COMPREPLY=( $(compgen -W "${paths}" -- ${cur}) )
+    return 0
+}
+
+complete -F _qcd qcd
+
+
+# Unload function definitions and autocompletion bindings
+if [ "$1" = "unload" ]
+then
+    unset -f _rsqueue
+    unset -f jcd
+    unset -f _jcd
+    unset -f qcd
+    unset -f _qcd
+
+    complete -A file rsqueue
+    complete -A file jcd
+    complete -A file qcd
+fi
+
